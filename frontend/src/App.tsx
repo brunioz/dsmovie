0523b7fdd67aa30f3af677e847824e9f59bee1cc
Bruno Guedes <bruniozguedes@gmail.com>
2022-01-11 22:41:09 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <h1>Página principal</h1>
-  );
+  return <NavBar></NavBar>;
 }
 
 export default App;
